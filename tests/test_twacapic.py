605fea6d09ed8c0c4cff4c153693d101c7fc6c42
FlxVctr <flxvctr@protonmail.com>
2021-03-16 19:51:35 +0100

    bump version

--- tests/test_twacapic.py
+++ tests/test_twacapic.py
@@ -16,7 +16,7 @@ from TwitterAPI.TwitterError import TwitterConnectionError
 
 
 def test_version():
-    assert __version__ == '0.1.4.6'
+    assert __version__ == '0.2.0'
 
 
 def test_can_create_credential_yaml(tmp_path):
